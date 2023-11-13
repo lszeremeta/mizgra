224628a2080292ab36b0da4bb09e28d0d47308e0
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2023-11-13 17:04:35 +0100

    Escape \ and " in attribute values for YARS-PG output

--- mizgra/outputs/yarspg.py
+++ mizgra/outputs/yarspg.py
@@ -7,21 +7,24 @@ from .output_format import OutputFormat
 class YARSPG(OutputFormat):
     def print_node(self, node_name: str, label: str, attributes: dict[str] = {}) -> None:
         # (hiddenL2 {"Text-Proper"}["articleid": "HIDDEN", "articleext": ".miz", "position": "17\5", "notation": "m.1.2", "uuid": "cac5245f-1eb1-566d-a3c2-001ac90a9d83"])
-        attribute_str: str = ", ".join([f'"{key}": "{value}"' for key, value in attributes.items()])
 
         if not attributes:
             print(f"({node_name} {{\"{label}\"}})")
         else:
+            # Replace \ with \\ and " with \" in attribute values using chr().
+            attribute_str: str = ", ".join(
+                [f'"{key}": "{value.replace(chr(92), chr(92) * 2).replace(chr(34), chr(92) + chr(34))}"' for key, value
+                 in attributes.items()])
             print(f"({node_name} {{\"{label}\"}}[{attribute_str}])")
 
     def print_relation(self, source_node: str, relation_name: str, target_node: str,
                        attributes: dict[str, str] = {}) -> None:
         # (xboole1L9566)-({"RELATION"})->(xboole1L9556)
-        attribute_str: str = ", ".join([f'"{key}": "{value}"' for key, value in attributes.items()])
 
         if not attributes:
             print(f"({source_node})-({{\"{relation_name}\"}})->({target_node})")
         else:
+            attribute_str: str = ", ".join([f'"{key}": "{value}"' for key, value in attributes.items()])
             print(f"({source_node})-({{\"{relation_name}\"}}[{attribute_str}])->({target_node})")
 
     def print_filename(self, filename: str) -> None:
